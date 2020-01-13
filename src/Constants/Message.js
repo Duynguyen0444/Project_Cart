8590d522862fcb99d933bc098b0d91689369e620
duynguyen0333 <duynguyen0333@gmail.com>
2020-01-13 14:25:47 +0700

    Redux structure

---
+++ src/Constants/Message.js
@@ -0,0 +1,5 @@
+export const MSG_ADD_TO_CART = "Mua hàng thành công ";
+export const MSG_UPDATE_CART = "Cập nhật giỏ hàng thành công";
+export const MSG_DELETE_PRODUCT_CART = "Xóa khỏi giỏ hàng thành công";
+export const MSG_EMPTY_CART = "Giỏ hàng rỗng";
+export const MSG_WELCOME = "Chào mừng tới shopping";
--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import appReducers from './Reducers/index';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(appReducers,)
 
+ReactDOM.render(
+  <Provider store={store}>
+  <App/>
+</Provider>, document.getElementById('root'));
 serviceWorker.unregister();
